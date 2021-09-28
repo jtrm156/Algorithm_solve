f2e37f774354f59e5f5316e78ff91c433958cd71
jtrm156 <skrk309@naver.com>
2021-09-28 23:28:28 +0900

    solving 3052

---
+++ src/backjoon_3052.kt
@@ -0,0 +1,11 @@
+import java.io.*
+import java.util.*
+
+fun main(){
+    val br = System.`in`.bufferedReader()
+    val bw = System.`out`.bufferedWriter()
+
+    for (i in 0 until 10) {
+        br.readLine()
+    }
+}
