c6ff330467953059d75bc30906e0a814b77a5e62
Yu Jae Hyeok <42407740+jtrm156@users.noreply.github.com>
2021-09-29 23:10:53 +0900

    solve 3052

--- src/backjoon_3052.kt
+++ src/backjoon_3052.kt
@@ -4,8 +4,27 @@ import java.util.*
 fun main(){
     val br = System.`in`.bufferedReader()
     val bw = System.`out`.bufferedWriter()
+    val list = mutableListOf<Int>()
+    var result = 0
 
     for (i in 0 until 10) {
-        br.readLine()
+        var n = br.readLine().toInt()
+        list.add(n%42)
     }
+
+    for (i in 0 until 10){
+        var count = 0
+
+        for (j in i+1 until 10){
+            if (list[i] == list[j]) {
+                count++
+            }
+        }
+
+        if (count == 0) {
+            result++
+        }
+    }
+
+    print(result)
 }
