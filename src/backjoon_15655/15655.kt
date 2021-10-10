cba39fcf15febab9051f4bbd50c60b86a7adb0fc
jtrm156 <skrk309@naver.com>
2021-10-10 23:22:51 +0900

    solve 15655...

--- src/backjoon_15655/15655.kt
+++ src/backjoon_15655/15655.kt
@@ -1,5 +1,15 @@
 package backjoon_15655
 
 fun main() {
+    val br = System.`in`.bufferedReader()
 
+    val (N, M) = br.readLine().split(" ").map{ it.toInt() }
+
+    val list = mutableListOf<Int>()
+
+    br.readLine().split(" ").map{ list.add(it.toInt())}
+
+    list.sort()
+
+    print(list)
 }
