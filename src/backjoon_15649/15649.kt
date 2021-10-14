1504b9e7e6e6056957dd16d897309ef1139f1a04
jtrm156 <skrk309@naver.com>
2021-10-14 22:31:27 +0900

    solve 13023...

--- src/backjoon_15649/15649.kt
+++ src/backjoon_15649/15649.kt
@@ -2,43 +2,40 @@ package backjoon_15649
 import java.io.*
 import java.util.*
 
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+var n = 0
+var m = 0
+val check = BooleanArray(10, { false })
+
+fun printString(sequence: String) {
+    for (i in 0..sequence.length - 1) {
+        bw.write("${sequence[i]} ")
+    }
+    bw.write("\n")
+}
+
+fun dfs(sequence: String) {
+    if (sequence.length >= m) {
+        printString(sequence)
+        return
+    }
+    for (i in 1..n) {
+        if (check[i]) continue
+        check[i] = true
+        dfs(sequence + "$i")
+        check[i] = false
+    }
+}
+
 fun main() {
 
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val token = StringTokenizer(br.readLine(), " ")
+    n = token.nextToken().toInt()
+    m = token.nextToken().toInt()
 
-    var n = 0
-    var m = 0
-    val check = BooleanArray(10, { false })
+    dfs("")
 
-    fun printString(sequence: String) {
-        for (i in 0..sequence.length - 1) {
-            bw.write("${sequence[i]} ")
-        }
-        bw.write("\n")
-    }
-
-    fun dfs(sequence: String) {
-        if (sequence.length >= m) {
-            printString(sequence)
-            return
-        }
-        for (i in 1..n) {
-            if (check[i]) continue
-            check[i] = true
-            dfs(sequence + "$i")
-            check[i] = false
-        }
-    }
-
-    fun main() {
-
-        val token = StringTokenizer(br.readLine(), " ")
-        n = token.nextToken().toInt()
-        m = token.nextToken().toInt()
-
-        dfs("")
-
-        bw.flush()
-    }
+    bw.flush()
 }
