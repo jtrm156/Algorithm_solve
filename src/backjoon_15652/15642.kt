56cecfe0b3e65d9554c32b38324dd7564e420727
jtrm156 <skrk309@naver.com>
2021-10-21 23:47:28 +0900

    solve 15652

---
+++ src/backjoon_15652/15642.kt
@@ -0,0 +1,39 @@
+package backjoon_15652
+
+import java.util.*
+
+var n = 0
+var m = 0
+val arr = IntArray(10)
+val isUsed = BooleanArray(10, { false })
+val br = System.`in`.bufferedReader()
+val bw = System.`out`.bufferedWriter()
+
+fun dfs(k : Int) {
+    if (k == m) {
+        for (i in 0 until m) {
+            //print("${arr[i]} ")
+            bw.write("${arr[i]} ")
+        }
+        //println()
+        bw.write("\n")
+        return
+    }
+
+    for (i in k .. n) {
+        arr[k] = i
+        isUsed[i] = true
+        dfs(k+1)
+    }
+}
+
+fun main() {
+    val token = StringTokenizer(br.readLine(), " ")
+    n = token.nextToken().toInt()
+    m = token.nextToken().toInt()
+
+    dfs(0)
+
+    bw.flush()
+    bw.close()
+}
