fc58ec4567ee6c803dc255aa85707b4fa27fbc3f
jtrm156 <skrk309@naver.com>
2021-10-24 09:13:16 +0900

    Create 15656

---
+++ src/backjoon_15656/15656.kt
@@ -0,0 +1,41 @@
+package backjoon_15656
+
+import java.util.*
+
+var n = 0
+var m = 0
+val arr = IntArray(10)
+val isUsed = BooleanArray(10, { false })
+val arr2 = mutableListOf<Int>()
+val br = System.`in`.bufferedReader()
+val bw = System.`out`.bufferedWriter()
+
+fun dfs(k : Int) {
+    if (k == m) {
+        for (i in 0 until m) {
+            //print("${arr[i]} ")
+            bw.write("${arr[i]} ")
+        }
+        //println()
+        bw.write("\n")
+        return
+    }
+
+    for (i in 0 until arr2.size) {
+        arr[k] = arr2[i]
+        dfs(k+1)
+    }
+}
+
+fun main() {
+    val token = StringTokenizer(br.readLine(), " ")
+    n = token.nextToken().toInt()
+    m = token.nextToken().toInt()
+
+    br.readLine().split(" ").map{ arr2.add(it.toInt()) }
+    arr2.sort()
+    dfs(0)
+
+    bw.flush()
+    bw.close()
+}
