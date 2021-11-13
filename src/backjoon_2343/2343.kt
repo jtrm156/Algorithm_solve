f3e90ee72963ab6348f7922c180676e1f8e327f1
jtrm156 <skrk309@naver.com>
2021-11-13 23:55:24 +0900

    Fix : 2343.kt

--- src/backjoon_2343/2343.kt
+++ src/backjoon_2343/2343.kt
@@ -2,29 +2,49 @@ package backjoon_2343
 
 lateinit var myArr: List<Int>
 
+var low = 0
+var high = 0
+
 fun main() {
     val br = System.`in`.bufferedReader()
     var (A,B) = br.readLine().split(" ").map{ it.toInt()}
     myArr = br.readLine().split(" ").map{ it.toInt() }
     var sum = 0
-    var start = 0
-    var end = 0
 
-    for (i in myArr){
-        sum += i
+    for (i in 0 until myArr.size){
+        sum += myArr[i]
+        low = Math.max(low, myArr[i])
     }
 
-    for (i in 0 until A) {
-        if(start < myArr[i]){
-            start = myArr[i]
+    high = sum
+
+    binarySearch(A,B)
+
+    print(low)
+}
+
+fun binarySearch(A : Int, B: Int) {
+
+    while (low <= high) {
+        var mid = (low + high) / 2
+        var sum = 0
+        var count = 0
+
+        for (i in 0 until A) {
+            if (sum + myArr[i] > mid) {
+               sum = 0
+               count++
+            }
+
+            sum += myArr[i]
         }
-        end+=myArr[i]
-    }
 
-    while(start <= end) {
-        var mid = (start+end)/2
-        if() {
-            answer = mid
+        if (sum > 0) count++
+
+        if (count <= B) {
+            high = mid -1
+        } else {
+            low = mid + 1
         }
     }
 }
