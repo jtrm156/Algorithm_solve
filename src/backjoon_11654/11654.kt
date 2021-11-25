c97970587192f4a2e1feb2079adb50a954951c20
jtrm156 <skrk309@naver.com>
2021-11-25 21:38:05 +0900

    Add 11654.kt

---
+++ src/backjoon_11654/11654.kt
@@ -0,0 +1,12 @@
+package backjoon_11654
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.`out`.bufferedWriter()
+
+    var str = br.readLine().toString()
+
+    for (i in str) {
+        println("${i.toInt()}")
+    }
+}
