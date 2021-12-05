98a0cd10b0918fdcb277eac1a0207d33466aa126
jtrm156 <skrk309@naver.com>
2021-12-05 22:14:31 +0900

    Fix 2292.kt

--- src/backjoon_2292/2292.kt
+++ src/backjoon_2292/2292.kt
@@ -2,9 +2,9 @@ package backjoon_2292
 
 // kotlin(native)
 // 시간초과
-fun main(args: Array<String>) {
-
-    var str = readLine().toString()
+fun main() {
+    val br = System.`in`.bufferedReader()
+    var str = br.readLine().toString()
     var A = str.split(" ").get(0).toLong()// 1000
     var B = str.split(" ").get(1).toLong() // 70
     var C = str.split(" ").get(2).toLong() // 170
@@ -16,7 +16,7 @@ fun main(args: Array<String>) {
 
     var minus:Long = A // 손해가 나는 금액
     var plus:Long = 0 // 이득이 나는 금액
-    var n:Int = 1 // 손익분기점에 도달할 횟수
+    var n = 1 // 손익분기점에 도달할 횟수
 
     while(true) {
         minus += B
