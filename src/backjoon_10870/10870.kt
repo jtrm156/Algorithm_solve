dea07a3b433d89e2a6d118076e55a3160b35582a
jtrm156 <skrk309@naver.com>
2021-12-08 23:32:26 +0900

    Fix 10870.kt

--- src/backjoon_10870/10870.kt
+++ src/backjoon_10870/10870.kt
@@ -1,5 +1,6 @@
 package backjoon_10870
 
+/*재귀
 fun main() {
     val br = System.`in`.bufferedReader()
     val input = br.readLine().toInt()
@@ -8,4 +9,21 @@ fun main() {
 
 fun fibonacci(N: Int): Int {
     return if (N < 2) N else fibonacci(N - 1) + fibonacci(N - 2)
+}
+*/
+
+/* 배열 */
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val input: Int = br.readLine().toInt()
+    val fibonacci = IntArray(input + 1)
+
+    for (i in fibonacci.indices) {
+        when (i) {
+            0 -> fibonacci[0] = 0
+            1 -> fibonacci[1] = 1
+            else -> fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
+        }
+    }
+    print(fibonacci[input])
 }
