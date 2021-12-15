7d4799c37361f41c61f40403ad6c9c770d216454
jtrm156 <skrk309@naver.com>
2021-12-15 23:47:05 +0900

    Add 9093.kt

---
+++ src/backjoon_9093/9093.kt
@@ -0,0 +1,15 @@
+package backjoon_9093
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    var n = readLine().toInt()
+    while(n-->0){
+        var sp = readLine().split(" ");
+        for(a in sp){
+            print(a.reversed()+' ')
+        }
+        println()
+    }
+}
