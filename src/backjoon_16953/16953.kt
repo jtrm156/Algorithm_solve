5081d6413ca78959d9b519026a3d286b04825f1c
jtrm156 <skrk309@naver.com>
2021-12-19 23:52:25 +0900

    Fix 16953.kt

--- src/backjoon_16953/16953.kt
+++ src/backjoon_16953/16953.kt
@@ -3,8 +3,9 @@ package backjoon_16953
 import java.util.*
 import kotlin.collections.HashSet
 val bw = System.`out`.bufferedWriter()
-fun main() = with(System.`in`.bufferedReader()){
-    var (A,B) = readLine().split(' ').map{it.toLong()}
+fun main(){
+    val br = System.`in`.bufferedReader()
+    var (A,B) = br.readLine().split(' ').map{it.toLong()}
     var st = HashSet<Long>()
     var q:Queue<Long> = LinkedList<Long>()
     q.add(A)
