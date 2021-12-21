6ce930b46f658ce2339f3d14d5e122ea2d791a82
jtrm156 <skrk309@naver.com>
2021-12-21 21:25:35 +0900

    Fix 10773.kt

--- src/backjoon_10773/10773.kt
+++ src/backjoon_10773/10773.kt
@@ -4,18 +4,22 @@ import java.io.*
 import java.util.*
 
 fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val br = System.`in`.bufferedReader()
+    val bw = System.`out`.bufferedWriter()
     val n = br.readLine().toInt()
     val stack = Stack<Int>()
+
     for(i in 0 until n) {
         val cmd = br.readLine().toInt()
         if(cmd==0) stack.pop()
         else stack.push(cmd)
     }
+
     var sum = 0
+
     while(!stack.empty())
         sum += stack.pop()
+
     bw.write("$sum")
     bw.flush()
     bw.close()
