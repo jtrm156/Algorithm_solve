1e5a6a0e7d51d3ac7ba71f1c194e754ce657b91c
jtrm156 <skrk309@naver.com>
2021-12-24 23:33:17 +0900

    Add 1373.kt

---
+++ src/backjoon_1373/1373.kt
@@ -0,0 +1,19 @@
+package backjoon_1373
+
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)){
+    var a = nextLine()
+    var sum = 0
+
+    if (a.length % 3 == 1) a = "00$a"
+    if (a.length % 3 == 2) a = "0$a"
+    // 3개씩으로 안나눠 떨어질 경우 앞에 0을 채워줌
+
+    for(i in a.indices step 3){
+        sum = a[i].toString().toInt() * 4 + a[i+1].toString().toInt() * 2 + a[i+2].toString().toInt()
+        // 3자리씩 끊어서 8진수로 변환
+        print(sum)
+    } // indices == 0 until a.length
+
+}
