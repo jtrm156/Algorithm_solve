4ce48ce882d20482c44a42b42bd578935f171180
jtrm156 <skrk309@naver.com>
2021-12-30 22:38:08 +0900

    Add 14405.kt

---
+++ src/backjoon_14405/14405.kt
@@ -0,0 +1,19 @@
+package backjoon_14405
+
+import kotlin.system.exitProcess
+
+fun main() {
+    var s= readLine()!!
+    while (true){
+        if(s.isEmpty()) break
+        s = when {
+            s.startsWith("pi")|| s.startsWith("ka") -> s.substring(2,s.length)
+            s.startsWith("chu") -> s.substring(3,s.length)
+            else ->{
+                print("NO")
+                exitProcess(0)
+            }
+        }
+    }
+    print("YES")
+}
