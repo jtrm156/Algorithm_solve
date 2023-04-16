523a1b789fcf60b6e5273db59cbf033fb1fe160f
jtrm156 <skrk309@naver.com>
2023-04-16 21:52:20 +0900

    "backjoon1001 solve"

--- src/backjoon_1001/backjoon1001.java
+++ src/backjoon_1001/backjoon1001.java
@@ -1,13 +1,27 @@
 package backjoon_1001;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class backjoon1001 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        /* scanner
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         int m = sc.nextInt();
+
+        System.out.print(n-m);
+        */
+
+        /* bufferedreader */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         System.out.print(n-m);
     }
