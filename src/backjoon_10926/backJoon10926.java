822908f2bc4e9aa4accdd7a28d0915d3d2e9d1d1
jtrm156 <skrk309@naver.com>
2023-04-19 23:12:09 +0900

    "backjoon10926 solve"

---
+++ src/backjoon_10926/backJoon10926.java
@@ -0,0 +1,13 @@
+package backjoon_10926;
+
+import java.util.Scanner;
+
+public class backJoon10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String lastString = "??!";
+
+        String s = sc.next();
+        System.out.println(s +lastString);
+    }
+}
