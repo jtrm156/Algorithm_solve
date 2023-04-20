6213588ea6ba74afc002744e957eaf75a2e8e00a
jtrm156 <skrk309@naver.com>
2023-04-20 20:30:37 +0900

    "backjoon2558 solve"

---
+++ src/backjoon_2558/backJoon2558.java
@@ -0,0 +1,19 @@
+package backjoon_2558;
+
+import java.util.Scanner;
+
+public class backJoon2558 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        sc.close();
+
+        System.out.println(A*(B%10));
+        System.out.println(A*(B%100/10));
+        System.out.println(A*(B/100));
+        System.out.println(A*B);
+    }
+}
